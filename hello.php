cbbd51dc420bc8bad431b31290f255d0981d53fb
Maria Feber <mafs18@student.bth.se>
2018-12-12 10:25:49 +0100

    Explain what changes I did

--- hello.php
+++ hello.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Hello WOrld</title>
+        <title>Hello World</title>
     </head>
     <body>
         <h1>Hello</h1>

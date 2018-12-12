50b055ebb55253d5dfeb36e4a6df32376f5a32a1
Maria Feber <mafs18@student.bth.se>
2018-12-12 09:35:24 +0100

    Adding new files to init project

---
+++ README.md
@@ -0,0 +1,4 @@
+THi is the readmefile.
+======================
+
+Describe project so all can download it and install.
---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello WOrld</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <?php echo "My name is MEGogo"; ?>
+
+    </body>
+</html>
